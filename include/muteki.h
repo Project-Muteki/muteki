0406c6c2bb7b18e96482bcf2f1fa926d6ff86801
dogtopus <dogtopus@users.noreply.github.com>
2026-08-30 02:15:33 +0900

    misc: Add missing ui includes to muteki.h

--- include/muteki.h
+++ include/muteki.h
@@ -33,10 +33,14 @@
 #include "muteki/ui/framework/event.h"
 #include "muteki/ui/framework/group.h"
 
+#include "muteki/ui/modals/filepicker.h"
+#include "muteki/ui/modals/messagebox.h"
+
 #include "muteki/ui/views/deskbox.h"
 
 #include "muteki/ui/widgets/button.h"
 #include "muteki/ui/widgets/controlmenu.h"
 #include "muteki/ui/widgets/menufield.h"
+#include "muteki/ui/widgets/slider.h"
 
 #endif // __MUTEKI_H__
