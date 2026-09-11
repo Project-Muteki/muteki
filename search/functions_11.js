7e9f270ae270acc5484bef2b55cf7b7dc523252f
dogtopus <dogtopus@users.noreply.github.com>
2026-09-11 17:50:49 +0000

    deploy: b81343e185b35ab93e02eefe75a6367b37bb17f8

--- search/functions_11.js
+++ search/functions_11.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['ui_5fbutton_5flayout_5ffrom_5ftext_5flayout_0',['ui_button_layout_from_text_layout',['../types_2button_8h.html#aafca709636e60b2023540b32b0d01580',1,'button.h']]],
-  ['ui_5fstatic_5fstyle_5foptions_1',['ui_static_style_options',['../types_2static_8h.html#a952cc570ed68b1b033ccdd5d267ce199',1,'static.h']]],
+  ['ui_5fstatic_5fstyle_5foptions_1',['ui_static_style_options',['../types_2static_8h.html#a7ed4cd716e89ffd2012d6d74e56b0217',1,'static.h']]],
   ['ui_5fstatic_5ftext_5foptions_2',['ui_static_text_options',['../widgets_2static_8h.html#a28031cd19422f5392556e87a60848f67',1,'static.h']]],
   ['unrelatedkeybutton_3',['UnRelatedKeyButton',['../widgets_2button_8h.html#a8803419f3b36f792c275d1da9fb1a473',1,'button.h']]],
   ['unstayresidentprograma_4',['UnStayResidentProgramA',['../loader_8h.html#ad87db8d02ec26f100e6579b6780614ef',1,'loader.h']]],
--- types_2static_8h.js
+++ types_2static_8h.js
@@ -2,11 +2,11 @@ var types_2static_8h =
 [
     [ "ui_static_s", "structui__static__s.html", "structui__static__s" ],
     [ "ui_static_t", "types_2static_8h.html#a903fc75a9e7f38c0f99516a09468b623", null ],
-    [ "ui_static_frame_type_e", "types_2static_8h.html#a4d0d46f64e027bd4f916b9c1d453974c", [
-      [ "UI_STATIC_FRAME_KIND_NONE", "types_2static_8h.html#a4d0d46f64e027bd4f916b9c1d453974ca0881425bb204ce92dfd8b9ee6e3fafc7", null ],
-      [ "UI_STATIC_FRAME_KIND_OUTLINE", "types_2static_8h.html#a4d0d46f64e027bd4f916b9c1d453974ca2eed888932333feb429b922268d2558d", null ],
-      [ "UI_STATIC_FRAME_KIND_SUNKEN", "types_2static_8h.html#a4d0d46f64e027bd4f916b9c1d453974ca5ec1416f2df8eb7ac1cec4fea69cdf8f", null ],
-      [ "UI_STATIC_FRAME_KIND_RAISED", "types_2static_8h.html#a4d0d46f64e027bd4f916b9c1d453974caca265502f4cef8f6a37f73ebf31dca3b", null ]
+    [ "ui_static_frame_kind_e", "types_2static_8h.html#a483f036efd82aecc3d460c59e98a5444", [
+      [ "UI_STATIC_FRAME_KIND_NONE", "types_2static_8h.html#a483f036efd82aecc3d460c59e98a5444a0881425bb204ce92dfd8b9ee6e3fafc7", null ],
+      [ "UI_STATIC_FRAME_KIND_OUTLINE", "types_2static_8h.html#a483f036efd82aecc3d460c59e98a5444a2eed888932333feb429b922268d2558d", null ],
+      [ "UI_STATIC_FRAME_KIND_SUNKEN", "types_2static_8h.html#a483f036efd82aecc3d460c59e98a5444a5ec1416f2df8eb7ac1cec4fea69cdf8f", null ],
+      [ "UI_STATIC_FRAME_KIND_RAISED", "types_2static_8h.html#a483f036efd82aecc3d460c59e98a5444aca265502f4cef8f6a37f73ebf31dca3b", null ]
     ] ],
-    [ "ui_static_style_options", "types_2static_8h.html#a952cc570ed68b1b033ccdd5d267ce199", null ]
+    [ "ui_static_style_options", "types_2static_8h.html#a7ed4cd716e89ffd2012d6d74e56b0217", null ]
 ];
